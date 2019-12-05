82c9e333bc751fc3482edbd1284234f51b41750c
Wellerson Costa - Dev <wellerson-costa-dev@outlook.com>
2019-12-04 20:07:38 -0500

    Projeto criado

---
+++ src/main/java/com/wellerson/ceos/CeosApplication.java
@@ -0,0 +1,13 @@
+package com.wellerson.ceos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wellerson/ceos/CeosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellerson.ceos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
