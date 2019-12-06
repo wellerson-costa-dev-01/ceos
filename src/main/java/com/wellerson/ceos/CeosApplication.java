7309aa0df15b364a99d8f4bd2712d6e1e9f900b7
Wellerson Costa - Dev <wellerson-costa-dev@outlook.com>
2019-12-06 18:11:32 -0500

    Criando operação de instanciação

--- src/main/java/com/wellerson/ceos/CeosApplication.java
+++ src/main/java/com/wellerson/ceos/CeosApplication.java
@@ -1,13 +1,36 @@
 package com.wellerson.ceos;
 
+
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.wellerson.ceos.repositories.TipoInstituicaoRepository;
+import com.wellerson.ceos.domain.TipoInstituicao;
+
+
 @SpringBootApplication
-public class CeosApplication {
+public class CeosApplication implements CommandLineRunner {
+  
+  @Autowired
+  private TipoInstituicaoRepository tipoInstituicaoRepository;
+  
 
 	public static void main(String[] args) {
 		SpringApplication.run(CeosApplication.class, args);
 	}
+  
+  @Override
+  public void run(String... args) throws Exception{
+    
+    TipoInstituicao tipoInst1 = new TipoInstituicao(null,"Centro Universitário",1);
+    TipoInstituicao tipoInst2 = new TipoInstituicao(null,"Faculdade",1);
+    
+    tipoInstituicaoRepository.saveAll(Arrays.asList(tipoInst1, tipoInst2));
+    
+  }
 
 }
